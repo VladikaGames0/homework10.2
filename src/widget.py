24a313b9f5b85c9fa5cc924bf8fabea3bf986edf
Владислав Ромашов <vromasvo5@gmail.com>
2025-08-19 19:03:35 +0700

    Создали новый модуль и функции карт

--- src/widget.py
+++ src/widget.py
@@ -1,1 +1,25 @@
+from typing import Union
+
+
+def get_mask_account(card_number: Union[str, int]) -> str:
+    """Принимает значение карты и возвращает название с замаскированным номером"""
+
+    s = str(card_number)
+    if s.startswith("Счет"):
+        card_name = "Счет"
+        number = s[len(card_name) :].strip()
+        return f"{card_name} **{number[-4:]}"
+
+    else:
+        parts = s.split()
+        card_name = " ".join(parts[:-1])
+        number = parts[-1]
+        masked = number[:6] + "*" * (len(number) - 10) + number[-4:]
+        formatted_number = " ".join(masked[i : i + 4] for i in range(0, len(masked), 4))
+        return f"{card_name} {formatted_number}"
+
+
+
+
+
 
